435c28cac53043d8341bc29c01eaaf7aae7978cd
Rumen Petrov <rumen.petrov@blubird.eu>
2019-06-12 09:28:43 +0300

    Pass the site url as argument and add minimist package to manage arguments

--- main.js
+++ main.js
@@ -2,16 +2,23 @@
 // https://github.com/GoogleChrome/puppeteer
 // https://www.scrapehero.com/how-to-take-screenshots-of-a-web-page-using-puppeteer/
 
+const argv = require('minimist')(process.argv.slice(2));
 const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 const iPhone = devices[ 'iPhone 8' ];
 const pixel = devices[ 'Pixel 2' ];
 
+if (!argv.url) {
+  console.error('url argument is required!');
+  console.error('Example: npm start -- --url="www.example.com"');
+  return false;
+}
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto('https://google.com/');
+  await page.goto(argv.url.toString());
 
   await page.setViewport({ width: 1920, height: 1080 });
   await page.screenshot({
